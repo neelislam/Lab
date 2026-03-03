bb3e2ed3c1e4fefa47148790739d9869f199108b
rabiul <rabiul17.ri@gmail.com>
2026-03-03 18:52:01 +0600

    Initial commit

---
+++ class_1/basic.cpp
@@ -0,0 +1,15 @@
+#include <graphics.h>
+#include <stdio.h>
+int main()
+{
+    // Creates a 640x480 window
+    initwindow(640, 480); // initialise graphics window
+
+    line(100, 100, 200, 200);  // draw a line
+    circle(100, 300, 30);       // draw a circle
+    rectangle(50, 200, 300, 400); // draw a rectangle
+
+    getch(); // wait for a key press
+    closegraph();               // close graphics window
+    return 0;
+}
